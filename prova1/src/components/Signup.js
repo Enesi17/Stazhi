6d11ce8652e42d314065c3f526891dcb5eaae12e
Enes Smajli <101655765+Enesi17@users.noreply.github.com>
2023-08-30 16:14:14 +0300

    Update Signup.js

--- prova1/src/components/Signup.js
+++ prova1/src/components/Signup.js
@@ -1,3 +1,7 @@
+//Created by  : Enes Smajli 
+//Date        : 05/09/23
+//Description : Sign up component, rensponsible for registring into databse new users 
+
 import React from 'react'
 import {Button, Card, Form} from 'react-bootstrap'
 const Signup = () => {
@@ -10,10 +14,6 @@ const Signup = () => {
             <Card.Body>
                 <Form>
                     <Form.Group>
-                        <Form.Label>ID</Form.Label>
-                        <Form.Control type='number' placeholder='Enter id'/>
-                    </Form.Group>
-                    <Form.Group>
                         <Form.Label>Email</Form.Label>
                         <Form.Control type='email' placeholder='Enter email'/>
                     </Form.Group>
@@ -22,9 +22,9 @@ const Signup = () => {
                         <Form.Control type='password' placeholder='Enter password'/>
                     </Form.Group>
                     <Form.Group>
-                        <Form.Label>firstName</Form.Label>
+                        <Form.Label>First Name</Form.Label>
                         <Form.Control type='text' placeholder='Enter first name'/>
-                        <Form.Label>lastName</Form.Label>
+                        <Form.Label>Last Name</Form.Label>
                         <Form.Control type='text' placeholder='Enter last name'/>
                     </Form.Group>
                     <Form.Group>
