f1b5b5095fb86f3f5b4d582ed130cd44ec25e3de
Enes Smajli <101655765+Enesi17@users.noreply.github.com>
2023-09-05 15:16:15 +0300

    The final form of the module
    This commit contains the last form of module. To be checked from the mentor...

---
+++ final_form/src/components/About.js
@@ -0,0 +1,56 @@
+//Created by  : Enes Smajli 
+//Date        : 05/09/23
+//Last Update : 11/09/23
+//Description : About component, short introduction
+
+import React from 'react';
+import "../index.css"
+
+const About = () => {
+  return (
+    <div>
+      <div className="container mt-5">
+        <div className="about">
+          <h2 className="display-4">T3-stazherat</h2>
+          <hr className="my-4" />
+          <div className="row">
+            <div>
+              <h4>First Module</h4>
+              <p>
+                This is the first module, a product of the realization of task 1 given on 09/04/2023. Required functionalities:
+              </p>
+              <ul>
+                <li>Sign up</li>
+                <li>Log in</li>
+                <li>Database (Firestore)</li>
+                <li>Authentication (Firebase)</li>
+                <li>Table view</li>
+                <li>Edit records/data</li>
+                <li>Add records/data</li>
+                <li>Remove/delete records/data</li>
+              </ul>
+
+              <p>
+                This task was given to the team of interns: Enes Smajli, Uveis Smajli. Product description: The idea is for the user to be able to register in the program, to be able to log in, to be able to see the data tables. The user can also edit, delete, or add new data to the application.
+              </p>
+
+              <p>
+                The application to register users (authentication) uses the services of Firebase, (<a href="https://firebase.google.com/" target="_blank" rel="noopener noreferrer">Firebase</a>). While as a database, temporarily, it uses Firestore, a product of Firebase.
+              </p>
+
+              <p>
+                In addition to those that have been requested, we have also added the possibility of resetting the password in case the user has forgotten it, but for this to happen, be careful! An active email must be used during registration! This is because the new password is sent to that email, which you write in the input corresponding to that part of the program.
+              </p>
+
+              <p>
+                For styling, we used the Bootstrap library, as requested.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ final_form/src/components/Login.js
@@ -0,0 +1,71 @@
+//Created by  : Enes Smajli 
+//Date        : 05/09/23
+//Last Update : 11/09/23
+//Description : Login component used to log in
+
+import React, { useRef, useState } from "react"
+import { Form, Button, Card, Alert } from "react-bootstrap"
+import { useAuth } from "../context/AuthContext"
+
+const Login = () => {
+
+    let  tell = true;
+    const { login } = useAuth();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+    
+        try {
+          setError("")
+          setLoading(true)
+          await login(emailRef.current.value, passwordRef.current.value)
+          window.location.pathname="/tables";
+        } catch {
+          setError("Failed to log in")
+        }
+    
+        setLoading(false)
+    }
+
+    function trueLogin() {
+        if(!tell){ window.location.pathname="/"}
+    }
+
+  return (
+    <div className="login">
+        <Card>
+            <Card.Header>
+                <h2>LOG IN</h2>
+            </Card.Header>
+            <Card.Body>
+                {error&&<Alert className='info' variant="danger">{error}</Alert>}
+                {!error&&<Alert className='info' variant="info">Welcom to our Login page</Alert>}
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group className='form-input-container'>
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type='email' ref={emailRef} placeholder='Enter email' required/>
+                    </Form.Group>
+                    <Form.Group className='form-input-container'>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type='password' ref={passwordRef} placeholder='Enter password' required/>
+                    </Form.Group>
+                    <Form.Group>
+                        <p>Did you<a href="/forgotPassword">forget your password?</a></p>
+                        <p>No account?<a href="/signup">Sign Up</a></p>
+                    </Form.Group>
+                    <Form.Group>
+                        <Button type='submit' className='button'>Log In</Button>
+                    </Form.Group>
+                </Form>
+            </Card.Body>
+        </Card>
+        
+    </div>
+  )
+}
+
+export default Login;
