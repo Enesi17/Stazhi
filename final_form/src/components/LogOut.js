515fbcebfc3d5232673b2a13ccdbe386d34c6f84
Enes Smajli <101655765+Enesi17@users.noreply.github.com>
2023-09-06 18:06:49 +0300

    Create LogOut.js
    This component was created because of the re-rendering in tables and navbar

---
+++ final_form/src/components/LogOut.js
@@ -0,0 +1,41 @@
+//Created by  : Enes Smajli 
+//Date        : 13/09/23
+//Last Update : 13/09/23
+//Description : LogOut.js component, it makes the loguot happen
+
+import React, {useState} from "react";
+import { useAuth } from "../context/AuthContext";
+import { Button, Table, Form, Container, Row, Col, Alert } from "react-bootstrap";
+
+const LogOut = () => {
+
+    const [error, setError] = useState("")
+    const { currentUser, logout } = useAuth();
+
+    async function handleLogout(event) {
+        event.preventDefault();
+        setError("");
+      
+        try {
+          await logout();
+          window.location.reload();
+          console.log("Logged out successfully...");
+        } catch {
+          setError("Failed to log out");
+          console.log("sdhfsjdfhjskdfhskjdfhsfjkshdfkjshdfkjshfdkjshfjksdhfkjsdhf")
+          
+        }
+     }
+
+    return ( 
+        <Row>
+          <Col>
+            <Button onClick={handleLogout}>
+              Log Out
+            </Button>
+          </Col>
+        </Row>
+     );
+}
+ 
+export default LogOut;
